2c9b4619918898ec6e4c649caf38435f90841f99
ina-uzu <ina.uzu.song@gmail.com>
2018-07-26 16:34:08 +0900

    using ProgressBar

--- app/src/main/java/com/example/ina97/prac_picasso/MainActivity.java
+++ app/src/main/java/com/example/ina97/prac_picasso/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.ina97.prac_picasso;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Transformation;
 
@@ -13,6 +16,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
         String url = "http://square.github.io/picasso/static/sample.png";
         ImageView img_picasso = (ImageView)findViewById(R.id.img);
@@ -20,6 +26,29 @@ public class MainActivity extends Activity {
                 .load(url)
                 .placeholder(R.mipmap.ic_launcher)
                 .error(R.mipmap.ic_launcher_round)
-                .into(img_picasso);
+                .into(img_picasso, new ImageLoadedCallback(progressBar){
+                    public void onSuccess(){
+                        if(this.progressBar!=null){
+                            this.progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+    private class ImageLoadedCallback implements Callback{
+        ProgressBar progressBar;
+        public ImageLoadedCallback(ProgressBar progBar){
+            progressBar=progBar;
+        }
+
+        @Override
+        public void onSuccess() {
+
+        }
+
+        @Override
+        public void onError() {
+
+        }
     }
 }
